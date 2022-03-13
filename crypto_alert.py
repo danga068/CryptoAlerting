c1bfab579d8cbde3db614d963d253ac936d1d234
danga068 <ashok.danga@shadowfax.in>
2022-03-14 00:28:20 +0530

    save'

--- crypto_alert.py
+++ crypto_alert.py
@@ -190,21 +190,23 @@ class CryptoAlert:
                 currency_detail_value = self.redis_client.get(currency_detail_key) or ""
                 current_price = float(current_price_data[currency])
                 if currency_detail_value:
-                    currency_detail_value = currency_detail_value.decode("utf-8") + ("->" + str(current_price))
+                    currency_detail_value = (str(current_price) + "<-") + currency_detail_value.decode("utf-8")
                 else:
                     currency_detail_value = str(current_price)
                 self.redis_client.setex(currency_detail_key, 12*60*60, currency_detail_value)
 
-                price_arr = currency_detail_value.split("->")
+                price_arr = currency_detail_value.split("<-")
+                low = min(price_arr)
+                high = max(price_arr)
                 if len(price_arr) > 60:
-                    start_records = 5
-                    last_records = 20
-                    currency_detail_value = "->".join(price_arr[:start_records])
+                    start_records = 20
+                    last_records = 5
+                    currency_detail_value = "<-".join(price_arr[:start_records])
                     currency_detail_value += " **** {} records *** ".format(len(price_arr)-start_records-last_records)
-                    currency_detail_value += "->".join(price_arr[-last_records:])
+                    currency_detail_value += "<-".join(price_arr[-last_records:])
 
-                message = "Coin {} change by {}% : {} -> {}, Data: {}".format(currency.replace("USDT", ""), str(diff_10_min), str(last_10_min_data[currency]), str(current_price), currency_detail_value)
-                print (message)
+                message = "{} changed {}% : L: {} H: {}, 10: {} Data: {}".format(currency.replace("USDT", ""), str(diff_10_min), last_10_min_data[currency] str(low), str(high), currency_detail_value)
+                # print (message)
                 PagerDuty().callPagerDuty(message)
 
 
